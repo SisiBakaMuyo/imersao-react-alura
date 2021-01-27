5f84b5fc8235664e7f9c9626e3df265187e716c4
Ísis A. Logullo <isis.logullo@usp.br>
2021-01-27 00:10:25 -0300

    alteracao da segunda aula

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>The legend of zelda</h1>
+            <h1>Quiz da Ghibli</h1>
           </Widget.Header>
           <Widget.Content>
             <form onSubmit={function (infosDoEvento) {
